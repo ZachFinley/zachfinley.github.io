d1d35c45f784e7c7abfd750714c1f61ce35423d5
ZachFinley <zcf5026@psu.edu>
2024-04-09 19:34:52 -0400

    react movie 7.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Movie.css';
+import Movie from './movie.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>List of Movies</h1>
+      <div className="Movies row">
+        <div className="col-lg-4">
+          <Movie  /> 
+        </div>
+        <div className="col-lg-4">
+          <Movie  /> 
+        </div>
+        <div className="col-lg-4">
+          <Movie  /> 
+        </div>
+      </div>
     </div>
   );
 }
+
 
 export default App;
